c8ae083648e78b62a368784303fcf7d9b03bf320
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2023-10-10 19:05:19 +0400

    lesson1 by nick nutsubidze

---
+++ Day 1/lesson1.py
@@ -0,0 +1,30 @@
+name= "nikolozi"
+
+
+surname= "nutsubidze"
+
+print(name)
+
+
+
+
+name = "nikolozi"
+age = 13
+height = 166
+
+knows_programming = False
+is_ugly = False
+
+isUgly = False
+
+print(name + " " + surname)
+
+#string is things that may placed in () 
+
+
+ #print(type(age))
+ #print(type(surname))
+
+print(name+ " "+ str(age))
+
+print(name+surname+str(age)(height)(knows_programming)(is_ugly)(isUgly))
